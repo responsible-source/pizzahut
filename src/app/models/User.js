61953f7187cdebddf5f3bbac9a4079d1c9c4fabd
Vincent Uchenna <viccystarks9878@yahoo.com>
2024-09-04 23:45:58 +0200

    new changes

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,20 +1,20 @@
-import {Schema} from "mongoose"
+// import {Schema} from "mongoose"
 
-const UserSchema = new Schema({
-    email: {
-        type: String, 
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        validate: pass =>{
-            if(!pass?.length || pass.lengh < 5){
-                new Error('password must be at least 5 character');
-            }
-        }
-    }
-}, {timestamps: true});
+// const UserSchema = new Schema({
+//     email: {
+//         type: String, 
+//         required: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         validate: pass =>{
+//             if(!pass?.length || pass.lengh < 5){
+//                 new Error('password must be at least 5 character');
+//             }
+//         }
+//     }
+// }, {timestamps: true});
 
-export const User = models?.User || model('User', UserSchema)
+// export const User = models?.User || model('User', UserSchema)
